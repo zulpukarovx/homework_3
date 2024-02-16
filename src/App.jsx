24609e0f213aaad7ad05dc5ffddbfd20dde9ad79
zulpukarovx <zulpukarovx@gmail.com>
2024-02-16 22:17:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    setTasks([...tasks, newTask]);
+    setNewTask('');
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Type your task"
+        />
+        <button onClick={addTask}>Add task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App
